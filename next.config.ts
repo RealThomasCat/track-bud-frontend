301cc8ec2e7559e37a6047055c80ccdb87a5fe87
RealThomasCat <yashveer.rtc@gmail.com>
2026-06-09 16:48:30 +0530

    feat: add nextjs standalone output config option

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+    /* config options here */
+    output: "standalone", // optimize production deployment by generating a self-contained server build.
+    reactCompiler: true,
 };
 
 export default nextConfig;
